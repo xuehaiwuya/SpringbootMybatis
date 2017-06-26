4ddb84a547ac84bfbbd979eed1a77efab9ecd37b
xuehaiwuya <panxiang1992@qq.com>
2017-06-26 13:42:49 +0800

    整合redis作为缓存数据库

--- src/main/java/com/panxiang/Application.java
+++ src/main/java/com/panxiang/Application.java
@@ -4,10 +4,17 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * springboot启动类
+ *
+ * Created by PanXiang on 2017-06-26.
+ */
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/com/panxiang/service/impl/UserServiceImpl.java
+++ src/main/java/com/panxiang/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.panxiang.dao.UserDao;
 import com.panxiang.domain.User;
 import com.panxiang.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 
@@ -16,8 +17,10 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
 
+    @Cacheable(value = "RedisCache",key = "'getUserByName_'+#name")
     @Override
     public User getUserByName(String name) {
+        System.out.println("数据库查询。。。");
         return userDao.getUserByName(name);
     }
 }
