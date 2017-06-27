1953837e5e84efe9f7f94b9fd3158e8b40ca6407
xuehaiwuya <panxiang1992@qq.com>
2017-06-28 00:21:21 +0800

    添加logback日志功能

--- src/main/java/com/panxiang/Application.java
+++ src/main/java/com/panxiang/Application.java
@@ -1,12 +1,13 @@
 package com.panxiang;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * springboot启动类
- *
+ * <p>
  * Created by PanXiang on 2017-06-26.
  */
 @SpringBootApplication
@@ -16,5 +17,12 @@ public class Application {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         SpringApplication.run(Application.class, args);
+
+        /**
+         * 关闭springboot启动Banner
+         */
+        // SpringApplication newRun= new SpringApplication(Application.class);
+        // newRun.setBannerMode(Banner.Mode.OFF);
+        // newRun.run(args);
     }
 }
